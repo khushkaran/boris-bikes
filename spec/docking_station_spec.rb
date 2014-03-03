61135b131dfc1a6017508a7d62d8c22035739336
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-03 14:25:53 +0000

    Refactored code/tests and does not accept more bikes if station is full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,10 @@ describe DockingStation do
   let(:bike) {Bike.new}
   let(:station) {DockingStation.new(:capacity => 20)}
 
+  def fill_station(station)
+    20.times {station.dock(Bike.new)}
+  end
+
   it "should accept a bike" do
     #bike = Bike.new
     #station = DockingStation.new
@@ -20,7 +24,12 @@ describe DockingStation do
 
   it "should know when it's full" do
     expect(station).not_to be_full
-    20.times {station.dock(bike)}
+    fill_station station
     expect(station).to be_full
+  end
+
+  it "should not accept a bike if it is full" do
+    fill_station station
+    expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
   end
 end
