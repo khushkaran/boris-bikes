15db4881ac58b29b1f8245dc8b93f110c9c04610
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-04 16:44:36 +0000

    Tests to ensure DockingStation doesn't exceed limits

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,18 @@ require_relative "../lib/docking_station"
 describe DockingStation do
   let(:station) {DockingStation.new(:capacity => 20)}
 
+  def fill_station
+    20.times {station.dock(Bike.new)}
+  end
+
   it "should allow setting default capacity on initialising" do
     expect(station.capacity).to eq(20)
   end
+
+  it "should not expect more than 20 bikes" do
+    expect(station).not_to be_full
+    fill_station
+    expect(station).to be_full
+  end
+
 end
