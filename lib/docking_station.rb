0fa95c0e8f630a0049d8bcc1f9e7f610d174c8b4
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-03-05 12:27:09 +0000

    Added extra tests for Docking Station to release/dock broken/working

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,4 +8,14 @@ class DockingStation
     self.capacity = fetched_capacity.is_a?(Integer) && fetched_capacity >= 0 ? fetched_capacity : DEFAULT_CAPACITY
   end
 
+  def accept_working_bikes(van)
+    put_in(van.available_bikes)
+    van.take_out(available_bikes)
+  end
+
+  def release_broken_bikes(van)
+    van.put_in(broken_bikes)
+    take_out(van.broken_bikes)
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require_relative "../lib/docking_station"
+require_relative "../lib/van"
 
 describe DockingStation do
   let(:station) {DockingStation.new(:capacity => 20)}
+  let(:van) {Van.new(:capacity => 50)}
+
+  def test_bikes
+    @working_bike, @broken_bike = Bike.new, Bike.new
+    @broken_bike.break
+  end
 
   def fill_station
     20.times {station.dock(Bike.new)}
@@ -15,6 +22,22 @@ describe DockingStation do
     expect(station).not_to be_full
     fill_station
     expect(station).to be_full
+  end
+
+  it "should only dock working bikes into docking station from van" do
+    test_bikes
+    van.dock(@working_bike)
+    station.accept_working_bikes(van)
+    expect(station.bikes).to eq([@working_bike])
+    expect(van.bikes.count).to eq(0)
+  end
+
+  it "should only release broken bikes into van from docking station" do
+    test_bikes
+    station.dock(@broken_bike)
+    station.release_broken_bikes(van)
+    expect(van.bikes).to eq([@broken_bike])
+    expect(station.bikes.count).to eq(0)
   end
 
 end
