7a1317ddaf55ee5badc2e466c8b58e17f4b68458
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-10 12:39:32 +0100

    Cleaned up initalize method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,8 +4,7 @@ class DockingStation
   include BikeContainer
 
   def initialize(options = {})
-    fetched_capacity = options.fetch(:capacity)
-    self.capacity = fetched_capacity.is_a?(Integer) && fetched_capacity >= 0 ? fetched_capacity : DEFAULT_CAPACITY
+    self.capacity = options[:capacity]
   end
 
   def accept_working_bikes(van)
--- lib/van.rb
+++ lib/van.rb
@@ -4,8 +4,7 @@ class Van
   include BikeContainer
 
   def initialize(options = {})
-    fetched_capacity = options.fetch(:capacity)
-    self.capacity = fetched_capacity.is_a?(Integer) && fetched_capacity >= 0 ? fetched_capacity : DEFAULT_CAPACITY
+    self.capacity = options[:capacity]
   end
 
   def deliver_broken_bikes(garage)
